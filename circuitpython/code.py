87d546314f7e9c969a7874553127322a167f0f0c
Ben <limhanyangb@gmail.com>
2022-02-13 10:04:39 +0800

    Added comments and tested first implementation of sending data

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -1,12 +1,52 @@
+# type: ignore
+
+import time
 import busio
 import board
 import notecard
+# from notecard import hub
 
-port = busio.I2C(board.SCL,board.SDA)
-nCard = notecard.OpenI2C(port,0,0)
+port = busio.UART(board.TX, board.RX, baudrate=9600)
+
+nCard = notecard.OpenSerial(port,debug=True)
+
+productUID = "com.gmail.limhanyangb:start"
 print("Notecard setup complete")
+# rsp = hub.set(nCard, productUID, mode="continuous", sync=True)
+req = {"req":"card.version"}
+
+
+rsp = nCard.Transaction(req)
+time.sleep(1)
+print(rsp)
+req = {}
+req = {"req":"hub.set","product":productUID}
+rsp = nCard.Transaction(req)
+time.sleep(1)
+
+print(rsp)
+req={}
 req = {"req":"note.add"}
 req["body"] = {"temp": 20.0}
 
 rsp = nCard.Transaction(req)
+time.sleep(1)
 print(rsp)
+
+req={}
+req= {"req":"hub.sync"}
+rsp=nCard.Transaction(req)
+time.sleep(1)
+print(rsp)
+
+req= {"req":"hub.sync.status"}
+rsp=nCard.Transaction(req)
+time.sleep(1)
+print(rsp)
+
+while(1):
+    time.sleep(3)
+    req= {"req":"hub.sync.status"}
+    rsp=nCard.Transaction(req)
+    print(rsp)
+    # print("In Loop")
